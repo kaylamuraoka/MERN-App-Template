99ea2e3a73e57841f15adb4c619ede978a917fc9
kaylamuraoka <kmurs98@gmail.com>
2021-03-03 16:16:13 -1000

    adding confirm model

---
+++ models/confirmModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const confirmSchema = new mongoose.Schema({
+  token: {
+    type: String,
+    required: true,
+  },
+  authorId: {
+    type: String,
+    required: true,
+  },
+});
